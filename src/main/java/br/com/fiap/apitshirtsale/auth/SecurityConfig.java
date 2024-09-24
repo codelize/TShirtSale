711a08a09eb8018f070fe2b51f0a01e5cb49399c
codelize <lflizier@gmail.com>
2024-09-24 00:48:44 -0300

    new

---
+++ src/main/java/br/com/fiap/apitshirtsale/auth/SecurityConfig.java
@@ -0,0 +1,39 @@
+package br.com.fiap.apitshirtsale.auth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.validation.annotation.Validated;
+
+@Configuration
+@Validated
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain config(HttpSecurity http, AuthorizationFilter authorizationFilter) throws Exception {
+        http.csrf(csrf -> csrf.disable());
+        http.authorizeHttpRequests(auth ->
+                auth
+                        .anyRequest().permitAll()
+        );
+        http.addFilterBefore(authorizationFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration)
+            throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+}
--- src/main/java/br/com/fiap/apitshirtsale/domain/TShirt/TShirtController.java
+++ src/main/java/br/com/fiap/apitshirtsale/domain/TShirt/TShirtController.java
@@ -3,6 +3,7 @@ package br.com.fiap.apitshirtsale.domain.TShirt;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/tshirts")
@@ -16,7 +17,7 @@ public class TShirtController {
 
     @GetMapping
     public List<TShirtResponse> getAllTShirts() {
-        return TShirtService.getAllTShirts().stream().map(TShirtResponse::fromModel).toList();
+        return TShirtService.getAllTShirts().stream().map(TShirtResponse::fromModel).collect(Collectors.toList());
     }
 
     @GetMapping("{id}")
